c8fa8fa5ac89f4f44c3898570ffa07b1ec0d3178
Velho008 <playergueico@gmail.com>
2025-06-20 21:05:44 -0300

    adicionei senha e adm nas propriedades do user, todas as mudanças necessarias foram feitas, criei um arquivo de excessoes, pros erros personalizados ficarem lá, falta um pouco de refino, mas tá bem util já

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -18,6 +18,9 @@ class UserController(BaseController): #herda de BaseController
         self.app.route('/users/edit/<user_id:int>', method=['GET', 'POST'], callback=self.edit_user) #Registra rota /users/edit/<user_id> com parâmetro inteiro para editar usuário específico
         self.app.route('/users/delete/<user_id:int>', method='POST', callback=self.delete_user) #Registra rota POST /users/delete/<user_id> para deletar usuário específico
 
+        #novas rotas para gerenciar privilegios
+        self.app.route('/users/promote/<user_id:int>', method='POST', callback=self.promote_user) #Promove usuário a admin
+        self.app.route('/users/demote/<user_id:int>', method='POST', callback=self.demote_user) #Remove privilégios de admin
 
     def list_users(self):
         users = self.user_service.get_all() #users é a lista de todos os users
@@ -29,8 +32,12 @@ class UserController(BaseController): #herda de BaseController
             return self.render('user_form', user=None, action="/users/add") #faz o formulario vazio para o novo usuario
         else:
             # POST - salvar usuário
-            self.user_service.save()
-            self.redirect('/users') #apos salvar manda de volta pra users
+            try:
+                self.user_service.save()
+                self.redirect('/users') #apos salvar manda de volta pra users
+            except ValueError as e:
+                # para tratar os erros
+                return self.render('user_form', user=None, action='/users/add', error=str(e))
 
 
     def edit_user(self, user_id): #serve pra editar um usuario existente, usa o id
@@ -42,14 +49,28 @@ class UserController(BaseController): #herda de BaseController
             return self.render('user_form', user=user, action=f"/users/edit/{user_id}")
         else:
             # POST - salvar edição
-            self.user_service.edit_user(user)
-            self.redirect('/users')
+            try:
+                self.user_service.edit_user(user)
+                self.redirect('/users')
+            except ValueError as e:
+                return self.render('user_form', user=user, action=f"/users/edit/{user_id}")
 
 
     def delete_user(self, user_id):
         self.user_service.delete_user(user_id) #remove o user
         self.redirect('/users') #redireciona 
 
+    def promote_user(self, user_id):
+        if self.user_service.promote_to_adm(user_id):
+            return self.redirect('/users')
+        else:
+            return "Erro ao promover usuario"
+    
+    def demote_user(self, user_id):
+        if self.user_service.demove_from_adm(user_id):
+            return self.redirect('/users')
+        else:
+            return "Erro ao promover usuario"
 
 user_routes = Bottle()
 user_controller = UserController(user_routes)
--- services/user_service.py
+++ services/user_service.py
@@ -1,9 +1,10 @@
 from bottle import request #usado pra acessar dados de formularios HTTP
 from models.user import UserModel, User
+from exceptions import EmailAlreadyUsedException
 
 class UserService:
     # Esta classe atua como uma camada intermediária entre o controlador (rotas)
-    #e o modelo de dados, implementando a lógica de negócio.
+    #e o modelo de dados, implementando a lógica de negócio.: 
 
     def __init__(self):
         self.user_model = UserModel() #acessa o usermodel de user.py
@@ -22,26 +23,79 @@ class UserService:
         birthdate = request.forms.get('birthdate')
         password = request.forms.get('password') #puxa a senha do formulario
         adm = request.forms.get('adm') == 'on' #é um checkbox, on se marcado, none se não 
+        
+        if not password: #verifica se não foi colocada uma senha 
+            raise ValueError("É obrigatorio ter uma senha")
+        
+        if self.user_model.get_by_email(email): #verifica se o email ja existe
+            raise EmailAlreadyUsedException(email)
 
-        user = User(id=new_id, name=name, email=email, birthdate=birthdate) #cria o objeto de user
+        user = User(id=new_id, name=name, email=email, birthdate=birthdate, password=password, adm=adm) #cria o objeto de user
         self.user_model.add_user(user) #adiciona na memoria
+
 
 
     def get_by_id(self, user_id):
         return self.user_model.get_by_id(user_id) #usa o metodo de user.py
+    
+    def get_by_email(self, email):
+        return self.user_model.get_by_email(email) #usa o metodo de user.py
 
 
     def edit_user(self, user): #usa um objeto user
         name = request.forms.get('name') #puxa os dados do formulario http
         email = request.forms.get('email')
         birthdate = request.forms.get('birthdate')
+        password = request.forms.get('password')
+        adm = request.forms.get('adm')== 'on'
+
+        existing_user = self.user_model.get_by_email(email) #verifica um user que já tem o mesmo email
+        if existing_user and existing_user.id != user.id: #caso o email seja usado por outro user, raiser error
+            raise EmailAlreadyUsedException(email)
 
         user.name = name #sobrescreve os dados
         user.email = email
         user.birthdate = birthdate
+        user.adm = adm
+
+        if password and password.strip(): #só troca de senha caso uma nova for fornecida
+            user.set_password(password)
 
         self.user_model.update_user(user) #usa o metodo do user.py
 
 
     def delete_user(self, user_id):
         self.user_model.delete_user(user_id) #usa o metodo do user.py
+
+    def authenticate(self, email, password):
+        #autentica um usuario usando email e senha
+        user = self.user_model.get_by_email(email)
+        if user and user.check_password(password): #caso a senha for correta
+            return user
+        return None #caso for incorreta
+    
+    def get_admins(self): #retorna apenas os admins
+        return self.user_model.get_admins()
+    
+    def get_regular_users(self): #retorna apenas usuarios comuns
+        return self.user_model.get_regular_users()
+    
+    def can_create_events(self, user): #verifica se o usuario pode criar eventos
+        return user and user.adm
+    
+    def promote_to_adm(self, user_id): #promove um user a adm, usando seu id
+        user = self.get_by_id(user_id)
+        if user:
+            user.adm = True
+            self.user_model.update_user(user)
+            return True
+        return False #caso não exista esse user
+    
+    def demove_from_adm(self, user_id): #remove adm de um user, usando seu id
+        user = self.get_by_id(user_id)
+        if user:
+            user.adm = True
+            self.user_model.update_user(user)
+            return True
+        return False #caso não exista esse user
+    
