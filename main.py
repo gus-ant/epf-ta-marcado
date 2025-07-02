37c0d424fd2bf186ec9137e4f450512f033c47b8
Velho008 <playergueico@gmail.com>
2025-07-02 11:24:28 -0300

    notas

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ if __name__ == '__main__':
 
 #criar os outros tipos de erro em exceptions.py
 
+#fazer a pesquisa existir
+
+#quando voltar continuar na coisa de sair do evento
+#e dos diferentes tipos de pagamentos
+
 #CRIEI UM ADM, FUI NA PAGINA DE EVENTOS E NÃO APARECEU A OPÇÃO DE FAZER EVENTO (SÓ QUANDO DERRUBA E VOLTA O SERVER)
 #TEMOS QUE MUDAR ONDE PUXA POR EMAIL PRA PUXAR POR ID (explicação):
 #- quando puxa por email, e se o user fizer a conta, apagar e fizer outra conta com o mesmo email, algumas infos podem ser puxadas do email antigo
 #- mas o id é sempre atualizado MESMO SE APAGAR A CONTA E FIZER OUTRA o id nunca vai ser o mesmo
-#- 
 
 #adicionar eventos no sistema:
 #adicionar opção de sair do evento
-#adicionar opção do dono excluir o evento
+#adicionar opção do dono excluir o evento (quando excluir o evento, aparecer pros users a opção de reembolso pendente)
 #-talvez adicionar outros tipos de ingresso (meia entrada e tals)
 #ter likes no evento (cada user só pode dar 1 like)
-#poder acessar pagamentos pendentes (talvez ter uma lista disso em /user)
 
 #BUG FIX
 #-não permitir mais que o user troque o email (ou quando ele trocar, todos os lugares onde aparece mudam tmb)
@@ -38,9 +41,7 @@ if __name__ == '__main__':
 #-verificar email
 #-recuperar senha caso esqueca
 #-usar likes como "algoritmo" pra mostrar os eventos com mais likes no topo do /home
-# colocar um método para modificar as infos dos eventos
 #--(podemos colocar como o clima vai estar no dia do evento)
-#--(podemos colocar qrcode)
 #quando um evento for apagado, mandar email pra todo mundo que se inscreveu E reembolsar
 
 
