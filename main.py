ede97e7a83eed191f719fe222315b3113397f16b
Velho008 <playergueico@gmail.com>
2025-07-06 23:15:13 -0300

    tirei a lista to-do

--- main.py
+++ main.py
@@ -3,14 +3,3 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
     app.run()
-    
-#LISTA TO-DO:
-#checar o que acontece quando deleta conta
-#{
-#adicionar opção do dono excluir o evento 🧧
-#}
-
-#metas pro projeto já pronto:
-#-fazer o issues
-#-fazer o readme e rever as metas do projeto no github do prof
-#-apagar o banco de dados inteiro e testar tudo que a gente vai apresentar META FINAL!!!
